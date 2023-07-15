23bdd2d229a15fb052b85a2dcb23352c4505081f
abdo-mhmd <abdo202224@gmail.com>
2023-07-15 07:34:48 -0700

    update 6 task

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(100):
     for x in range(i+1, 10):
         if i == 8 and x == 9:
             print("{:d}{:d}".format(i, x), end="\n")
-        else:    
+        else:
             print("{:d}{:d},".format(i, x), end=" ")
